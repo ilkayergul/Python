1201f3aba47f5096b0fb354b896a3085d455edce
ilkayergul <ilkay35ergul@gmail.com>
2021-01-09 23:02:19 +0300

    Update 001-Aritmetik İşlemler.py

--- Syntax/001-Aritmetik İşlemler.py
+++ Syntax/001-Aritmetik İşlemler.py
@@ -3,17 +3,13 @@ b = 2
 
 # Bölme işlemi küsuratlı olarak verir.
 sonuc = a/b 
-print(sonuc)
 
 # Bölme işlemini sonucunu küsuratsız verir.
 sonuc1 = a//b 
-print(sonuc1)
 
 # Bölme işleminin kalan değerini verir
 sonuc2 = a%b
-print(sonuc2)
 
 # Bellekteki son değeri verir
 sonuc3 = _
-print(sonuc3)
 
