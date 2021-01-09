531ebe3b63c594ac0340da36d0bc8188f67eeea6
ilkayergul <ilkay35ergul@gmail.com>
2021-01-10 01:13:44 +0300

    Update 010-Fonksiyonlar.py

--- Syntax/010-Fonksiyonlar.py
+++ Syntax/010-Fonksiyonlar.py
@@ -1,18 +1,15 @@
 def kare(x):
   print(x**2)
-  
-kare(3) # 9
+kare(3)
 
 # Return Kullanımı
 def kare(x):
   return x**2
-  
 sonuc = kare(3)
 
 # Birden fazla parametreli kullanımı. Parametreler default tanımlı.
 def insan(yas='21', kilo=75):
   print("Yaş:",yas)
   print("Kilo:",kilo)
-  
 insan()
 
