941c9e9c04e11942daeab476874359b78069e9a9
ilkayergul <ilkay35ergul@gmail.com>
2021-01-09 23:12:32 +0300

    Create 002-Print Fonksiyonu.py

---
+++ Syntax/002-Print Fonksiyonu.py
@@ -0,0 +1,17 @@
+#Ekrana yazdırma işlemi yapar.
+print('İlkay Ergül')
+
+#Tırnak işaretlerini virgül ile ayrılabilir. Boşluk görevi görür.
+print('İlkay','Ergül') #İlkay Ergül
+
+#Artı işareti birleştirme yapar.
+print('İlkay','Ergül') #İlkayErgül
+
+#String ve int ifadeler virgül ile ayrılarak yazılabilir.
+print('Yaşım:',24) #Yaşım: 24
+
+#Virgül koyulan kısımlara değer atama.
+print('Adım','İlkay', sep='---') #Adım---İlkay
+
+#Çıktının sonuna değer atama
+print('Adım','İlkay', end='---') #Adım İlkay ---
