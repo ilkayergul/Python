2232a927dd5412f0241acc1810f5a221acb72ba5
ilkayergul <ilkay35ergul@gmail.com>
2021-01-10 00:49:39 +0300

    Create 008-Range Fonksiyonu.py

---
+++ Syntax/008-Range Fonksiyonu.py
@@ -0,0 +1,11 @@
+#range(1,9,2) --> Parametre(başlangıç, bitiş, artış miktarı)
+
+# 0 dan 5 e kadar olan sayıları yazdırır. 5 dahil değildir.
+for x in range(5):
+  print(x)
+  
+# 1 den başlayıp 11 e kadar 2 şer artırıp sayıları ekrana yazar.
+for x in range(1,11,2):
+  print(x)
+  
+
