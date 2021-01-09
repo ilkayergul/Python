875f3464fd7656688fbeb60a1faac56b18189b71
ilkayergul <ilkay35ergul@gmail.com>
2021-01-10 01:32:12 +0300

    Create 012-Sözlükler(Dictionary).py

---
+++ Syntax/012-Sözlükler(Dictionary).py
@@ -0,0 +1,29 @@
+# Sözlük -> Key, Value
+
+nesne = {'Yukseklik':'20', 'Genislik':'10'}
+
+# Key değeri üzerinden value değerine erişilir.
+print(nesne['Yukseklik'])
+print(nesne['Genislik'])
+
+# Key değerine göre düzenleme yapılır.
+nesne['Yukseklik'] = '30'
+
+# Key değerini silme
+del nesne['Yukseklik']
+
+# Sözlüğü temizleme. İçerisini boşaltır. Bellekte adresini tutar.
+nesne.clear()
+
+# Sözlüğü silme.
+del nesne
+
+
+
+
+
+
+
+
+
+
