547244028e0eb83bb9477c10e976a3aed958f7d0
ilkayergul <ilkay35ergul@gmail.com>
2021-01-09 23:26:26 +0300

    Create 003-String İfadeler.py

---
+++ Syntax/003-String İfadeler.py
@@ -0,0 +1,13 @@
+# String ifadeler tek tırnak veya çift tırnak içerisinde yazılır.
+
+# Kaçış karakteri(\) kullanılarak tek tırnak içerisinde yazma.
+sonuc = 'Merhaba, İzmir\'e selamlar.'
+
+# Çift tırnak içerisinde yazma.
+sonuc = "Merhaba, İzmir'e selamlar"
+
+# String ifadelerde çarpma işlemi.
+sonuc = "Merhaba" * 2  # MerhabaMerhaba 
+
+# String ifadelerde toplama işlemi.
+sonuc = "Merhaba" + "İzmir" # Merhabaİzmir
