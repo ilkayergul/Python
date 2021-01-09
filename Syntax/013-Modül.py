89053440408fc029674a0cdb5fbce9faedf2b1df
ilkayergul <ilkay35ergul@gmail.com>
2021-01-10 01:42:25 +0300

    Create 013-Modül.py

---
+++ Syntax/013-Modül.py
@@ -0,0 +1,13 @@
+# Modüller bir fonksiyonun import edilerek kullanılmasını sağlar.
+
+# Fonksiyon tanımlama
+def karsilamaMesaj(ad, soyad):
+  return "Hoşgeldiniz " + ad + " " + soyad
+
+# Farklı sayfalarda kullanmak için import edilir.
+import karsilamaMesaj
+  
+
+  
+
+  
