8762d9f66e0ba2828e9a8fea7d127523d0345046
hasanOzyavru <34573227+hasanOzyavru@users.noreply.github.com>
2021-01-10 16:20:30 +0300

    Update 007-For Döngüsü.py

--- Syntax/007-For Döngüsü.py
+++ Syntax/007-For Döngüsü.py
@@ -8,3 +8,8 @@ for x in sehirler:
 for harf in 'İlkay Ergül':
   if harf == ' ':
     break
+    
+#List comprehension - square of even numbers
+nums = [1,2,3,4,5,6,7]
+numsqr = [x*x for x in nums if x%2==0]
+print(numsqr)
