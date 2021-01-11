2f2d81b82a0969d4622b200b4f8e30f95b388342
ilkayergul <ilkay35ergul@gmail.com>
2021-01-12 00:57:05 +0300

    Create 002-Default Değerli Dizi Oluşturma.py

---
+++ Numpy/002-Default Değerli Dizi Oluşturma.py
@@ -0,0 +1,9 @@
+# Sıfırlardan oluşan dizi oluşturma
+zeros = np.zeros((3,4)) #[[0., 0., 0., 0.],[0., 0., 0., 0.],[0., 0., 0., 0.]]
+
+# Birlerden oluşan dizi oluşturma
+ones = np.ones((3,4))
+
+# Boş değerlerden oluşan dizi oluşturma.
+# ??? Pythonda değer sıfıra yakın ise boş olarak kabul edilir.
+empties = np.empty((3,4))
