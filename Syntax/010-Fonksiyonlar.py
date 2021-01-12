8afef860918e8fc22e59320a9e7f576f89a36da9
ilkayergul <ilkay35ergul@gmail.com>
2021-01-13 00:05:27 +0300

    Update 010-Fonksiyonlar.py

--- Syntax/010-Fonksiyonlar.py
+++ Syntax/010-Fonksiyonlar.py
@@ -13,3 +13,25 @@ def insan(yas='21', kilo=75):
   print("Kilo:",kilo)
 insan()
 
+# Parametre sayısı belli değil ise * kullanılır.
+# Parametre dizi gibi davranır. Parametreye index yardımıyla erişilir.
+def students(*student):
+  print("student[2]")
+
+# Parametre sırasına göre girilmek istenmiyor ise (key,value) üzerinden erişim sağlanır.
+def students(student1, student2, student3):
+  return student1 + student2 + student3
+
+students(student3 = "Mehmet", student2 = "Ali", student1 = "Veli")
+
+# Parametrenin başına ** eklenirse dictionary gibi davranır.
+def students(**student):
+  print("Öğrencinin adı: " + student["name"])
+  
+students(age=20, name="Ahmet")
+
+# Fonksiyon tanımlandığında içi boş bırakılamaz. Eğer içini sonradan doldurmak istersek pass komutu kullanılır.
+def calc_kdv(x):
+  pass
+
+ 
